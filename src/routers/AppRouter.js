e19deba24da04327566ab96b4320dcf6d6885810
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-23 13:31:09 +0200

    Router for Mini Portfolio

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 
-
+import Home from '../components/Home';
+import Contact from '../components/Contact';
+import Portfolio from '../components/Portfolio';
+import PortfolioItem from '../components/PortfolioItem';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
@@ -11,6 +14,10 @@ const AppRouter = () => (
         <div>
             <Header />
             <Switch>
+                <Route path="/" component={Home} exact={true} />
+                <Route path="/portfolio" component={Portfolio} exact={true}/>
+                <Route path="/portfolio/:id" component={PortfolioItem} />
+                <Route path="/contact" component={Contact} />
                 <Route component={NotFoundPage} />
             </Switch>
         </div>
