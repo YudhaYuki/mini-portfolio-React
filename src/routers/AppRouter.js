d8b9234039567f6360d6bc173190189d56a72571
YudhaYuki <YudhaYuki@hotmail.com>
2018-05-23 11:59:46 +0200

    First commit

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+
+import NotFoundPage from '../components/NotFoundPage';
+import Header from '../components/Header';
+
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+    </BrowserRouter>
+)
+
+export default AppRouter;
